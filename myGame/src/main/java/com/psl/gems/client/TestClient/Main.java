545745c9460b3e788fb51d4070d9486921b0fbf5
Pratiksha Ved <pratikshaved421@gmail.com>
2023-04-22 19:43:46 +0530

    changed main.java

--- myGame/src/main/java/com/psl/gems/client/TestClient/Main.java
+++ myGame/src/main/java/com/psl/gems/client/TestClient/Main.java
@@ -7,6 +7,7 @@ import com.psl.gems.Entities.Player;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.io.FileWriter = new java.io.FileWriter("cardgame.html");
 
 public class Main {
     public static void main(String[] args) {
@@ -59,6 +60,43 @@ public class Main {
 
         Game game = new Game();
         game.play(player1 , player2, deck);
+
+    StringBuilder html = new StringBuilder();
+    html.append("<!DOCTYPE html>\n");
+    html.append("<html lang=\"en\">\n");
+    html.append("<head>\n");
+    html.append("<title>Card Game</title>");
+    html.append("<link rel = \"stylesheet\" href=\"cardgame.css\">\n");
+    html.append("<script src=\"cardgame.js\"></script>\n");
+    html.append("</head>\n");
+    html.append("<body>\n");
+    html.append("<div class = \"container\">\n");
+    int i=0;
+    for (Player player : players) {
+      html.append("<div class=\"player\">\n");
+      html.append("<div class=\"player-name\"><h2>" + player.getName() + "</h2></div>\n");
+      html.append("<div class = \"cards\">\n");
+      for (Card card : player.getCardsInHand()) {
+        html.append("<div class=\"card \" id = \"" + card.getSuit() + card.getRank() + "\" onclick=\"flipCard(this.id," + i + ")\">\n");
+        html.append("<div class=\"card-inner\">\n");
+        html.append("<div class=\"card-back\"><img src=\"images/back.jpeg\" alt=\"back of card\"></div>\n");
+        // html.append("<div class=\"card-back\"><img src=\"images/" + card.getSuit() + card.getRank() + ".png\" alt=\"front of card\" >\n");
+        html.append("<div class=\"card-front\"><img src= \"" card.getPictureName()"></div>\n");
+
+
+        html.append("</div></div></div>\n");
+        
+      }
+      html.append("</div>\n");
+      html.append("<button class=\"show-highest-card\" disabled id=\"btn"+ i + "\" onclick = \" showHighestCard(" + player.showHighestCard().getSuit() + ")\">Show Highest Card</button>\n");
+      html.append("</div>\n");
+      i++;
+    }
+
+    html.append("<button class=\"decide-winner\" disabled id = \"btnwinner\" onclick=\"decideWinner(btnwinner)\">Decide Winner</button>\n");
+    html.append("</div>\n");
+    html.append("</body>\n");
+    html.append("</html>\n");
 
 
 
